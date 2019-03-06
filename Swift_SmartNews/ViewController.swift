e2fd7b2b066c1de1c900d3bf6a8e0dafca8d8c7b
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-03-06 14:11:03 +0900

    Initial Commit

---
+++ Swift_SmartNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_SmartNews
+//
+//  Created by Naoki Arakawa on 2019/03/06.
+//  Copyright © 2019 Naoki Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
